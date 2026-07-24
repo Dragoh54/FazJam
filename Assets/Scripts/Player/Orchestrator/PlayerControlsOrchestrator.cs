a2193e187383e1dc6271c6dbaa5fd1890db7628a
Anton Log <Antuandelog@outlook.com>
2026-07-24 17:17:43 +0300

    feat: zoom out for player and Lobby room prefab

--- Assets/Scripts/Player/Orchestrator/PlayerControlsOrchestrator.cs
+++ Assets/Scripts/Player/Orchestrator/PlayerControlsOrchestrator.cs
@@ -3,23 +3,40 @@ using UnityEngine;
 
 public class PlayerControlsOrchestrator : MonoBehaviour
 {
+    private SpriteRenderer _spriteRenderer;
     private IInputHandler[] _playerInputHandlers;
+
+    [SerializeField]
+    private Sprite _playerIconOnZoomOut;
+
+    private Sprite _prevSprite;
 
     public bool IsBlocked { get; set; }
 
     private void Awake()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _playerInputHandlers = GetComponents<IInputHandler>();
     }
 
     private void Update()
     {
-        if (!IsBlocked)
+        if (IsBlocked && !_prevSprite)
         {
-            foreach (var inputHandler in _playerInputHandlers)
-            {
-                inputHandler.HandleInput();
-            }
+            _prevSprite = _spriteRenderer.sprite;
+            _spriteRenderer.sprite = _playerIconOnZoomOut;
+
+            return;
+        } 
+        else if (!IsBlocked && _prevSprite)
+        {
+            _spriteRenderer.sprite = _prevSprite;
+            _prevSprite = null;
+        }
+
+        foreach (var inputHandler in _playerInputHandlers)
+        {
+            inputHandler.HandleInput();
         }
     }
 }
